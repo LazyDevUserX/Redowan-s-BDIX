6a2a9bacbfb5aafdfb8034cab38a37aacabfe93c
LazyDevUserX <163187136+LazyDevUserX@users.noreply.github.com>
2025-06-27 10:04:11 -0900

    Update FTPBDPlugin.kt

--- BdixFTPBD/src/main/kotlin/com/redowan/FTPBDPlugin.kt
+++ BdixFTPBD/src/main/kotlin/com/redowan/FTPBDPlugin.kt
@@ -1,1 +1,13 @@
-nope
+package com.redowan.BdixFTPBD
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class FTPBDPlugin: Plugin() {
+    override fun load(context: Context) {
+        // All providers should be added here
+        registerMainAPI(FTPBDProvider())
+    }
+}
