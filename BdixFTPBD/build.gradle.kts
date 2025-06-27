dd104cebf060c285743f1385541587bf51f85ab9
LazyDevUserX <163187136+LazyDevUserX@users.noreply.github.com>
2025-06-27 09:48:33 -0900

    Update build.gradle.kts

--- BdixFTPBD/build.gradle.kts
+++ BdixFTPBD/build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
+    id("com.lagradost.cloudstream3.provider")
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
 }
+
+// The version of your plugin
+version = 1
 
 android {
     namespace = "com.redowan.bdixftpbd" // Unique namespace for the plugin
@@ -28,4 +32,35 @@ android {
 
 dependencies {
     // You can add specific dependencies here if needed
+}
+
+// Cloudstream metadata block
+cloudstream {
+    description = "A BDIX FTP provider for Cloudstream. Works even during internet shutdowns."
+    authors = listOf("YourName") // TODO: Change to your name or handle
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1 // will be 3 if unspecified
+
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "Anime",
+        "AnimeMovie",
+        "OVA",
+        "Cartoon",
+        "AsianDrama",
+        "Others",
+        "Documentary",
+    )
+    language = "bn"
+
+    // TODO: Add a URL to a direct image link for the plugin icon if you have one.
+    // iconUrl = "http://example.com/icon.png"
 }
